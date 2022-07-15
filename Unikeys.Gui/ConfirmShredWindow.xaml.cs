58d578dff3e2caa80985ea8b3999ec3a9fd2f31f
Aeris <aeris.github@gmail.com>
2022-07-15 20:17:01 +0200

    Made a custom confirmation box for shredding

---
+++ Unikeys.Gui/ConfirmShredWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace Unikeys.Gui;
+
+public partial class ConfirmShredWindow : Window
+{
+    public bool Confirmed { get; private set; }
+
+    public ConfirmShredWindow()
+    {
+        Confirmed = false;
+        InitializeComponent();
+        ButtonTimer();
+    }
+
+    private void CancelButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private async void ButtonTimer()
+    {
+        var i = 5;
+        while (i > 0)
+        {
+            ProceedIn.Text = "Proceed in " + i + " seconds...";
+            await Task.Delay(1000);
+            i--;
+        }
+        ProceedIn.Text = "Proceed";
+        ConfirmButton.IsEnabled = true;
+    }
+
+    private void ConfirmButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Confirmed = true;
+        Close();
+    }
+}
