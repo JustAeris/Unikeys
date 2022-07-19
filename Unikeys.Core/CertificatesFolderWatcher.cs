a4f708e9b793f3fdcc5f0b83e2d855e0ae396fa8
Aeris <aeris.github@gmail.com>
2022-07-19 11:38:31 +0200

    Added a file watcher to update the certificates list IRT

---
+++ Unikeys.Core/CertificatesFolderWatcher.cs
@@ -0,0 +1,67 @@
+﻿namespace Unikeys.Core;
+
+public sealed class CertificatesFolderWatcher
+{
+    public IList<FileInfo> CertificatesList { get; private set; }
+    private readonly string _certificatesFolder = Path.Combine(Directory.GetCurrentDirectory(), "Certificates");
+
+    private FileSystemWatcher? FileSystemWatcher { get; set; }
+
+    public CertificatesFolderWatcher()
+    {
+        CertificatesList = Directory.GetFiles(_certificatesFolder, "*.pfx", SearchOption.AllDirectories)
+            .Select(s => new FileInfo(s)).ToList();
+
+        FileSystemWatcher = new FileSystemWatcher
+        {
+            Path = _certificatesFolder,
+            NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.CreationTime,
+            Filter = "*.pfx",
+            IncludeSubdirectories = false,
+            EnableRaisingEvents = true
+        };
+
+        FileSystemWatcher.Created += OnFileChanged;
+    }
+
+    public event EventHandler<CertificateListUpdatedEventArgs>? CertificatesListUpdated;
+
+    public void Start()
+    {
+        if (FileSystemWatcher == null) return;
+        FileSystemWatcher.EnableRaisingEvents = true;
+
+        while (true)
+        {
+            var unused = FileSystemWatcher.WaitForChanged(WatcherChangeTypes.All);
+            OnFileChanged(null!, EventArgs.Empty);
+        }
+
+        // ReSharper disable once FunctionNeverReturns
+    }
+
+    private void OnFileChanged(object sender, EventArgs e)
+    {
+        var dic = Directory.GetFiles(_certificatesFolder, "*.pfx", SearchOption.AllDirectories)
+                             .Select(s => new FileInfo(s)).ToList();
+
+        CertificatesList = dic;
+
+        OnCertificatesListUpdated(new CertificateListUpdatedEventArgs(dic));
+    }
+
+    private void OnCertificatesListUpdated(CertificateListUpdatedEventArgs e)
+    {
+        CertificatesListUpdated?.Invoke(this, e);
+    }
+}
+
+public class CertificateListUpdatedEventArgs : EventArgs
+{
+    public List<FileInfo> CertificatesList { get; }
+
+    public CertificateListUpdatedEventArgs(List<FileInfo> certificatesList)
+    {
+        CertificatesList = certificatesList;
+    }
+}
