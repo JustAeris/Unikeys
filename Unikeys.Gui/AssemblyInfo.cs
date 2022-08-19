5f9f2280dc2a03f171895eae18f41e0c26493a51
Aeris <aeris.github@gmail.com>
2022-08-19 22:07:37 +0200

    Changed version number to reflect the new fixes

--- Unikeys.Gui/AssemblyInfo.cs
+++ Unikeys.Gui/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Windows;
     // app, or any theme specific resource dictionaries)
 )]
 // The program will pull this version to display in the "About" tab
-[assembly: AssemblyVersion("1.2.4.*")]
+[assembly: AssemblyVersion("1.2.5.*")]
